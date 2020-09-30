8ac1a7940ff1ff8204458761cda5e14858303e29
dafma <mar_tp@hotmail.es>
2020-09-30 13:14:20 -0500

    el constrains define el ancho maximo de la pantalla

--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:curso_rest_api_loginv2/utils/responsive.dart';
 import 'package:flutter/material.dart';
 import 'input_text.dart';
 
@@ -9,53 +10,59 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  
 
   @override
   Widget build(BuildContext context) {
+    final Responsive responsive = Responsive.of(context);
     return Positioned(
       bottom: 30,
-      left: 20,
-      right: 20,
-        child: Column(
-        children:<Widget>[
-          InputText(
-            keyboardType: TextInputType.emailAddress,
-            label: "EMAIL ADDRESS"),  
-          Container(
-            decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(
-                color: Colors.black12
-              ))
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                    child: InputText(
-                    obscureText: true,
-                    borderEnabled: false,
-                    label: "PASSWORD"
-                  ),
-                ),
-                FlatButton(
-                  padding: EdgeInsets.symmetric(vertical:10),
-                  onPressed: (){}, 
-                  child: Text("Forgot the Password", style: TextStyle(fontWeight: FontWeight.bold)))
-              ],
-            ),
+        child: Container(
+          // con el contrains decimos que tendremos una ancho mazimo de 400
+          constraints: BoxConstraints(
+            maxWidth: 400,
           ),
-          SizedBox(height:20),    
-          SizedBox(
-            width: double.infinity,
-              child: FlatButton(
-                //pading al button
-              padding: EdgeInsets.symmetric(vertical:15),  
-              child: Text("Sign In", style: TextStyle(color: Colors.white),),
-              onPressed: (){},
-              color: Colors.pinkAccent
+          child: Column(
+          children:<Widget>[
+            InputText(
+              keyboardType: TextInputType.emailAddress,
+              label: "EMAIL ADDRESS"),  
+            Container(
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  color: Colors.black12
+                ))
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: InputText(
+                      obscureText: true,
+                      borderEnabled: false,
+                      label: "PASSWORD"
+                    ),
+                  ),
+                  FlatButton(
+                    padding: EdgeInsets.symmetric(vertical:10),
+                    onPressed: (){}, 
+                    child: Text("Forgot the Password", style: TextStyle(fontWeight: FontWeight.bold)))
+                ],
+              ),
             ),
-          )    
-        ]
+            SizedBox(height:20),    
+            SizedBox(
+              width: double.infinity,
+                child: FlatButton(
+                  //pading al button
+                padding: EdgeInsets.symmetric(vertical:15),  
+                child: Text("Sign In", style: TextStyle(color: Colors.white, fontSize: responsive.dp(2.5)),),
+                onPressed: (){},
+                color: Colors.pinkAccent
+              ),
+            )    
+          ]
       ),
+        ),
     );
   }
 }
